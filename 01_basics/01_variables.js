325c6abe6f6b3a8bbd16a51f087de7ef1c578024
Aditya Singh Parihar <adityaparihaarr@gmail.com>
2025-12-12 09:55:01 +0000

    variables and comments

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountID = 234897
+let accountName = "Aditya"
+var accountEmail = "01@aditya.com"
+accountCity = "Satna"
+
+let accountState
+
+console.table([accountID, accountName, accountEmail, accountCity, accountState]);
+
+// never use var keyword
+// because of its issue in block scope and functional scope
