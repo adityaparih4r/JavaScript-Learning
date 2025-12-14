515d09636c442f94d9f35df34dfc3e189f556568
Aditya Singh Parihar <adityaparihaarr@gmail.com>
2025-12-14 13:14:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "Aditya"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // datatype - undefined
+console.log(typeof null); // datatype - object
